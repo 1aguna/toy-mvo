93282e45866b317cc6c4051d6274ab4b3498ff60
1aguna <mlusher28@gmail.com>
2020-04-06 12:37:51 -0700

    Update constants.h

--- include/constants.h
+++ include/constants.h
@@ -1,12 +1,5 @@
-//
-// Created by laguna on 3/26/20.
-//
-
 #pragma once
 const double PIXEL_NOISE = 1.0;
-const double OUTLIER_RATIO = 0.0;
-const bool ROBUST_KERNEL = true;
-const bool STRUCTURE_ONLY = false;
 const bool DENSE = false;
 const int OPTIMIZE_COUNT = 70;
 const int MAX_FEATURES = 2000;
